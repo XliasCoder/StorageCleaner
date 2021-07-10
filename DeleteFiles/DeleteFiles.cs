aca7bfa2ba1b8568665bdfaad49d481f8c8a6011
Elias Shamoun <Elias Shamoun@DESKTOP-1VD0A0E>
2021-07-10 16:21:37 +0300

    Add project files.

---
+++ DeleteFiles/DeleteFiles.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeleteFiles
+{
+    public partial class DeleteFiles : ServiceBase
+    {
+        private System.Timers.Timer timer;
+        public int timerTick;
+        public DeleteFiles()
+        {
+            InitializeComponent();
+            double defaultTimerDelay = Convert.ToDouble(System.Configuration.ConfigurationManager.
+                                AppSettings["defaultTimerDelay"]);
+            timer = new System.Timers.Timer(defaultTimerDelay);
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(WorkProcess);
+            //WorkProcess(null, null);
+        }
+        public void WorkProcess(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            string errorMsg = "";
+            try
+            {
+                
+                timer.Enabled = true;
+                timerTick++;
+                //timer.Interval = 60000;
+                string path = Path.Combine("C:\\Users\\Elias Shamoun\\source\\repos\\DeleteFiles\\DeleteFiles", "test");
+                DirectoryInfo di = new DirectoryInfo(path);
+                foreach(FileInfo fi in di.GetFiles())
+                {
+                    fi.Delete();
+                }
+                foreach (DirectoryInfo dir in di.GetDirectories())
+                {
+                    dir.Delete(true);
+                }
+                string LogPath = "C:\\Users\\Elias Shamoun\\source\\repos\\DeleteFiles\\DeleteFiles\\Log.txt";
+                using (StreamWriter writer = new StreamWriter(LogPath, true))
+                {
+                    writer.WriteLine(string.Format("Windows Service Called on " + DateTime.Now.ToString("dd /MM/yyyy hh:mm:ss tt") + " " + timer + " " + timerTick));
+                    writer.Close();
+                }             
+            }
+            catch (Exception ex)
+            {
+                errorMsg = ex.Message;
+                string LogPath = "C:\\Users\\Elias Shamoun\\source\\repos\\DeleteFiles\\DeleteFiles\\Log.txt";
+                using (StreamWriter writer = new StreamWriter(LogPath, true))
+                {
+                    writer.WriteLine(string.Format("Windows Service Exception: " + errorMsg + " "+ DateTime.Now.ToString("dd /MM/yyyy hh:mm:ss tt") ));
+                    writer.Close();
+                }
+                timer.Enabled = true;
+            }
+
+        }
+        protected override void OnStart(string[] args)
+        {
+            timer.Enabled = true;
+        }
+        protected override void OnStop()
+        {
+            timer.Enabled = false;
+        }
+    }
+}
---
+++ DeleteFiles/ProjectInstaller.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace DeleteFiles
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.deleteFilesProcessor = new System.ServiceProcess.ServiceProcessInstaller();
+            this.deleteFiles = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // deleteFilesProcessor
+            // 
+            this.deleteFilesProcessor.Account = System.ServiceProcess.ServiceAccount.LocalSystem;
+            this.deleteFilesProcessor.Password = null;
+            this.deleteFilesProcessor.Username = null;
+            // 
+            // deleteFiles
+            // 
+            this.deleteFiles.ServiceName = "DeleteFiles";
+            this.deleteFiles.Description = "Delete Files Service Demo";
+            this.deleteFiles.ServiceName = "DeleteFiles";
+            
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.deleteFilesProcessor,
+            this.deleteFiles});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller deleteFilesProcessor;
+        private System.ServiceProcess.ServiceInstaller deleteFiles;
+    }
+}
